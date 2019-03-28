6265aa352336f0da37aaabc642f6f7b05c9c2bdc
Charles-Antoine Francisco <charlantfr@gmail.com>
2019-03-28 14:17:42 -0400

    update messaging.py + templates/index.html

--- server/project/api/messaging.py
+++ server/project/api/messaging.py
@@ -15,39 +15,37 @@ def index():
     return render_template("index.html")
 
 
-@messaging_blueprint.route('/close/<room>')
-def close(room):
-    close_room(room, namespace='/private')
-    return '<h1>Room closed!</h1>'
-
-
-@socketio.on('message from user', namespace='/messages')
+@socketio.on("message from user", namespace="/messages")
 def receive_message_from_user(message):
-    print('USER MESSAGE: {}'.format(message))
-    emit('from flask', message.upper(), broadcast=True)
+    emit("from flask", message, broadcast=True)
 
 
-@socketio.on('username', namespace='/private')
+@socketio.on("username", namespace="/private")
 def receive_username(username):
-    users[username] = request.sid
-    print('Username added!')
+    #users[username] = request.sid
+    print("Username added!")
 
 
-# payload = {'username' : recipient, 'message' : message_to_send}
-@socketio.on('private_message', namespace='/private')
+# payload = {"username" : recipient, "message" : message_to_send}
+@socketio.on("private_message", namespace="/private")
 def private_message(payload):
-    recipient_session_id = users[payload['username']]
-    message = payload['message']
-    emit('new_private_message', message, room=recipient_session_id)
+    recipient_session_id = users[payload["username"]]
+    message = payload["message"]
+    emit("new_private_message", message, room=recipient_session_id)
 
 
-@socketio.on('join_room', namespace='/private')
+@socketio.on("join_room", namespace="/private")
 def handle_join_room(room):
     join_room(room)
-    emit('room_message', 'a new user has joined', room=room)
+    emit("room_message", "a new user has joined", room=room)
 
 
-@socketio.on('leave_the_room', namespace='/private')
+@socketio.on("leave_the_room", namespace="/private")
 def handle_leave_room(room):
     leave_room(room)
-    emit('room_message', 'a user has left the room', room=room)
+    emit("room_message", "a user has left the room", room=room)
+
+
+@socketio.on('disconnect_me', namespace='/private')
+def disconnect_me(message):
+    disconnect()
